2006c03d41abda2105dc68928a7fc16e8c7814c7
gogopowercoding <furaihanalharits@gmail.com>
2025-06-03 14:24:22 +0700

    p

---
+++ src/aplikasi/AplikasiSistemPerpajakanKendaraan.java
@@ -0,0 +1,16 @@
+package aplikasi;
+
+/**
+ *
+ * @author Advan
+ */
+public class AplikasiSistemPerpajakanKendaraan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
